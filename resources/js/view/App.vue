105fe4b491644ab5fc29fc7042303462c3c15b83
Allan Gadelha <allangadelha@gmail.com>
2020-11-12 23:28:43 -0300

    Components

---
+++ resources/js/view/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <router-link :to="{ name: 'home' }">Home</router-link>
+        <router-link :to="{ name: 'hello' }">Hello</router-link>
+        <router-link :to="{ name: 'example' }">Example</router-link>
+
+        <div class="container">
+            <p>Laravel SPA com Vue JS</p>
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+
+
+}
+</script>
